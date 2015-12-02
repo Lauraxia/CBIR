9ff400fe14b1588a1faa7e31921a871474a820e2
Laura McCrackin <laura.mccrackin@gmail.com>
2015-12-02 14:50:19 -0500

    Taking only first colour channel of 3-channel greyscale images where all channels are equal.

--- CBIR/getSurfFeatures.m
+++ CBIR/getSurfFeatures.m
@@ -6,10 +6,18 @@ files = dir(path);
 i=1;
 for file = files'
    irma{i} = imread(sprintf('%s/%s', dirpath, file.name));
+   
+   %any 3-channel images seem to be the same for all colour channels, so we
+   %can just remove all but channel 1:
+   if (length(irma{i}(1,1,:)) == 3)
+       irma{i}(:,:,2:3) = [];
+   end
+   %TODO: filter the 3-channel greyscale images to 1-channel, and make sure
+    %nothing is actually colour, or we'll need to handle that differently
+   
    i = i+1;
 end
-%TODO: filter the 3-channel greyscale images to 1-channel, and make sure
-%nothing is actually colour, or we'll need to handle that differently
+
 
 %%
 
@@ -20,5 +28,5 @@ for file = files'
    features{i} = detectSURFFeatures(irma{i}, 'MetricThreshold', 200);
    strongestfeatures{i}=features{i}.selectStrongest(10);
    i = i+1;
-   fprintf('%d\n', i);
+   fprintf('Calculating features for %d \r', i);
 end
