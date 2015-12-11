2eca622cab59ea34bbab7edb725a3ad48e2d92a2
Wajiha <swajiham@yahoo.com>
2015-12-11 16:34:56 -0500

    Added a tally to keep track of the consensus between image features and a lookup table to indicate which feature belongs to which image. Tally needs to be fixed

--- CBIR/getSurfFeatures.m
+++ CBIR/getSurfFeatures.m
@@ -34,6 +34,7 @@ for file = files'
 end
 
 %% calculate SURF features for them (using a low enough threshold to guarantee a min number of features to use)
+numSURF=10;
 features = cell(1, length(files));
 strongestfeatures = cell(1, length(files));
 fprintf('Progress:\n');
@@ -43,7 +44,7 @@ fprintf(['\n' repmat('.',1,(floor(length(files)/100))) '\n\n']);
 %guarantee a min number of features to use)
 parfor i=1:length(files)
    SURFfeatures{i} = detectSURFFeatures(irma{i}, 'MetricThreshold', 200);
-   strongestSURFfeatures{i}=SURFfeatures{i}.selectStrongest(10);
+   strongestSURFfeatures{i}=SURFfeatures{i}.selectStrongest(numSURF);
 
    %fprintf('Calculating features for %d \n', i);
    if mod(i,100) == 0
@@ -73,12 +74,12 @@ for file = files'
 end
 
 
-%% saving features to array for input to lsh
+%% saving features from training images to array for input to lsh
 n=1;
 
-for i = 1:5
+for i =1:trainingLength
     
-    for j=1:length(strongestSURFfeatures{i})
+    for j=1:strongestSURFfeatures{i}.Count
         
         currFeat=strongestSURFfeatures{i}(j);
         
@@ -86,31 +87,88 @@ for i = 1:5
             double(currFeat.Orientation); double(currFeat.Location(1));...
             double(currFeat.Location(2)); double(currFeat.Metric)] ;
         
-        j=j+1;
+        %lookup table to keep track of which features belong to which
+        %image 
+        featInd(n,1:2) = [i n];
         n=n+1;
     end
-    i=i+1;
+    
+
 end
+
+%% saving features from testing images to array 
+% this needs to be fixed!!
+
+% n=1;
+% 
+% for i = trainingLength+1:testingLength+trainingLength
+%     
+%     for j=1:length(strongestSURFfeatures{i})
+%         
+%         currFeat=strongestSURFfeatures{i}(j);
+%         
+%         testFeat(:,n)=[double(currFeat.Scale); double(currFeat.SignOfLaplacian);...
+%             double(currFeat.Orientation); double(currFeat.Location(1));...
+%             double(currFeat.Location(2)); double(currFeat.Metric)] ;
+%         
+%        
+%         n=n+1;
+%     end
+% end
 
 
 %% creating lsh data structure for input features
-Te=lsh('e2lsh', 50,30,size(inputFeat,1), inputFeat, 'range', 255, 'w', -4);
+Te=lsh('e2lsh', 50,25,size(inputFeat,1), inputFeat, 'range', 255, 'w', -3);
 
 %% providing query feature to lsh to find closest matches 
-[iNN,numcand]=lshlookup(inputFeat(:,8),inputFeat,Te,'k',11);
+
+rNN=21;
+j=1;
+tally=zeros((rNN)*numSURF,2);
+%read in csv file provided for IRMA database as a table data structure
+filepath='../../IRMA/2009/IRMA Code Training/ImageCLEFmed2009_train_codes.02.csv';
+t=readtable(filepath, 'Delimiter', ';');
+
+%convert table to structured array 
+c=table2struct(t(:,1:2));
+
+for m=1:1
+[iNN,numcand]=lshlookup(inputFeat(:,m),inputFeat,Te,'k',rNN);
 
 %read image indexes of closest matches
 for i=1:length(iNN)
-    imgnum(i)=floor(iNN(i)/10)+1;
-    i=i+1;
-end
+    imgnum(i)=featInd(find(iNN(i)==featInd(:,2)),1);
+    
+%keep a tally of images matched to each feature of input image 
+%currently not working, giving an error 'Index exceeds matrix dimensions'
+    
+% index=find((c(imgnum(i)).image_id==tally(:,1)))
+%  
+%     if isempty(index)==0
+%         tally(index,2)=tally(index,2)+1;
+%     else
+%         tally(j,:)=[c(imgnum(i)).image_id; 1];
+%         j=j+1;
+%       
+%     end
+%     
+
+end;
+    
+%extractIRMAcode function needs to be modified
 
 %extracting IRMA codes of the closest matches obtained through LSH by
 %providing indexes and path to csv file containing IRMA codes, and writing
 %them to a file 
-filepath= '../../IRMA/2009/Irma Code Training/ImageCLEFmed2009_train_codes.02.csv';
+%extractIRMAcode(c, imgnum);
+%type test.txt
 
-extractIRMAcode(filepath, imgnum);
+end;
+
+
+
+
+
     
     
 
